a853b202003adc07fafcc75a1f3a90a0c781c2a8
Ernesto Cisnero <miguelernestocisnero@gmail.com>
2025-11-20 20:19:26 -0500

    Initial Commit

---
+++ LaunchTrack/LaunchTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchTrackApp.swift
+//  LaunchTrack
+//
+//  Created by Ernesto Cisnero on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct LaunchTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
