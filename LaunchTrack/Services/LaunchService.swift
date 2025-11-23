f03d0f83a949e39d5fa802e5c347368100a0876b
Ernesto Cisnero <miguelernestocisnero@gmail.com>
2025-11-22 19:17:31 -0500

    V1.0.0

--- LaunchTrack/Services/LaunchService.swift
+++ LaunchTrack/Services/LaunchService.swift
@@ -6,8 +6,6 @@
 //
 
 import Foundation
-/* https://lldev.thespacedevs.com */ //For developing stage: No calls rate limit
-/* "https://ll.thespacedevs.com/2.2.0/launch/upcoming/"  */ // For production 15 calls per hour
 class LaunchService {
     private let url = URL(string: "https://ll.thespacedevs.com/2.2.0/launch/upcoming/")!
 
