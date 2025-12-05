bc9ab6241d21d1f0614adfb917cf3bec208a228b
Ernesto Cisnero <miguelernestocisnero@gmail.com>
2025-12-04 23:21:57 -0500

    Added the notification system, bell button, and some UI improvements.

--- LaunchTrack/Services/LaunchService.swift
+++ LaunchTrack/Services/LaunchService.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class LaunchService {
-    private let url = URL(string: "https://ll.thespacedevs.com/2.2.0/launch/upcoming/")!
+    private let url = URL(string: "https://lldev.thespacedevs.com/2.2.0/launch/upcoming/")!
 
     func fetchUpcomingLaunches() async throws -> [Launch] {
         let (data, _) = try await URLSession.shared.data(from: url)
